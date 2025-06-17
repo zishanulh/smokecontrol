1adc1d5404208f26598dce7fa6087471e085980b
Zishan Hussain <109183765+zishanulh@users.noreply.github.com>
2025-06-17 12:59:31 -0400

    Create MassFlowSI.m

---
+++ MassFlowSI.m
@@ -0,0 +1,12 @@
+% Orifice equation for design of pressurized smoke control systems.
+% Equation 3.4 in Handbook of Smoke Control Engineering (2nd ed.)
+% Klote, J. H., Milke, J. A., Turnbull, P. G., Kashef, A., Phillips, D. A., & Ferreira, M. J. (2024). Handbook of Smoke Control Engineering (2nd ed.). ASHRAE.
+
+
+% Function: Calculates mass flow rate - SI
+% m [kg/s], C [], A [m^2], rho [kg/m^3], delP [Pa]
+function    m = MassFlowSI(C,A,rho,delP)
+
+    m = C*A*((2*rho*delP)^0.5);
+
+end
