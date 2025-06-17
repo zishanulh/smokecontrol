2e62c8fe597c3c8b241121923807773e7d985bd8
Zishan Hussain <109183765+zishanulh@users.noreply.github.com>
2025-06-17 17:30:25 -0400

    Update MassFlow.m
    License Update

--- MassFlow.m
+++ MassFlow.m
@@ -1,3 +1,8 @@
+% Copyright (c) 2025 Zishan Hussain
+% Licensed under CC BY-NC 4.0: https://creativecommons.org/licenses/by-nc/4.0/
+% Non-commercial use only.
+
+
 % Orifice equation for design of pressurized smoke control systems.
 % Equation 3.4 in Handbook of Smoke Control Engineering (2nd ed.)
 % Klote, J. H., Milke, J. A., Turnbull, P. G., Kashef, A., Phillips, D. A., & Ferreira, M. J. (2024). Handbook of Smoke Control Engineering (2nd ed.). ASHRAE.
