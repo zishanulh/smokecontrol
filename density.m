e046e42919f8b420c8ed32fc1dfa098d0f7596ab
Zishan Hussain <109183765+zishanulh@users.noreply.github.com>
2025-06-21 07:26:01 -0400

    Create density.m

---
+++ density.m
@@ -0,0 +1,17 @@
+% Copyright (c) 2025 Zishan Hussain
+% Licensed under CC BY-NC 4.0: https://creativecommons.org/licenses/by-nc/4.0/
+% Non-commercial use only.
+
+
+% Equation to calculate the density smoke/air from ideal gas law.
+% Equation 3.5 in Handbook of Smoke Control Engineering (2nd ed.)
+% Klote, J. H., Milke, J. A., Turnbull, P. G., Kashef, A., Phillips, D. A., & Ferreira, M. J. (2024). Handbook of Smoke Control Engineering (2nd ed.). ASHRAE.
+
+
+% Function: Calculates Density
+% rho [lb/ft^3], p [psi], T [°R], R=53.34 ft*lbf*lbm/R
+function    rho = density(p,T)
+
+    rho = (144*p)/(53.34*T);
+
+end
